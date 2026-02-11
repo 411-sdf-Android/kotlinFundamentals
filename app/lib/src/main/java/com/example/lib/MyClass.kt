698857cb0568661cc05b6ccf15bad62ce3a44d30
SaulFnck <sauld5354@gmail.com>
2026-02-11 17:42:34 -0600

    Initial comit

---
+++ app/lib/src/main/java/com/example/lib/MyClass.kt
@@ -0,0 +1,73 @@
+package com.example.lib
+
+fun main(){
+   /* println("hola mundo");
+
+    val price: Double = 13.0;
+    val porcentaje: Double = .10;
+    val tip: Double = price * porcentaje;
+    val total: Double = price * tip;
+
+    println("El costo de la comida es: $$total")
+*/
+
+    /*
+    * EL portero de la discoteca verifica que una persona pueda ingresar a la disco YEAH solo si es mayor de edad.
+    * */
+
+//    println("Dime tu edad.")
+//    val input = readlnOrNull(); //Ingresar dato desde consola
+//    val age = input?.toIntOrNull();//Si input no es null ejecuta "toInt()"
+
+//    if(age == null){
+//        println("El número que ingresaste no es numerico entero, intentalo bien MENSA")
+//    }else if(age > 18){
+//        println("Eres mayor de edad, puedes ingresar")
+//    }else{
+//        println("Haste pa alla pinche morrillo")
+//    }
+//    when(age) {
+//        null -> println("El número que ingresaste no es numerico entero, intentalo bien MENSA")
+//        in 18.. Integer.MAX_VALUE -> println("Eres mayor de edad, puedes ingresar")//Rango de numeros
+//        else -> println("Haste pa alla pinche morrillo")
+//    }
+    //Valor a infinito Integer.MAX_VALUE
+
+//    //Ciclo for
+//    for (i in 10..10){
+//        println(i)
+//    }
+//
+//    for(i in 10 downTo 2){
+//        println(i)
+//    }
+//
+//    for(i in 100 downTo 0 step 10){
+//        println(i)
+//    }
+
+    //Ciclo WHILE
+//    var counter: Int = 12;
+//
+//    while(counter < 20){
+//        println("El contador es menor que 20")
+//        counter++
+//    }
+//
+//    do{
+//        println("El contador vale menos que 30")
+//        counter++
+//    }while(counter< 30)
+
+    //Operadores logicos: && ||
+    //Operadores logicos + - * / %
+
+
+    //Determinar si es numero primo
+
+    println("Dime un número")
+    val input= readlnOrNull()
+    val number = input?.ToIntOrNull()
+
+
+}
